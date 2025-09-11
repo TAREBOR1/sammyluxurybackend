1094df3d56fbb289da995a79eab0cd3633ee585b
tarebor <onodaifetareborgift@gmail.com>
2025-09-11 17:09:04 +0100

    finally

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 
 app.use(cors({
-    origin:'https://sammyrental.netlify.app/',
+    origin:'https://sammyluxuryhome.netlify.app',
     methods:['GET','POST','PUT','DELETE'],
     allowedHeaders:[
         'Content-Type',
