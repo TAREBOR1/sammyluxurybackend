cc022b05d90b430e0817cf6993db109fd3ed541e
tarebor <onodaifetareborgift@gmail.com>
2025-09-12 16:17:20 +0100

    test

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const database=require('./config/dbconfig')
 const app=require('./app')
 
 
+
+app.get('/health',(req,res)=>{
+     res.json({
+        success:true,
+        message:'backend is live'
+     })
+})
+
+
 app.listen(port,()=>{
     console.log("server is connecting to:",port)
 })
